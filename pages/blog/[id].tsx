2c6e7965bcb3b5c30809cd9e8bfcebb12f2b1a57
Philani Ncube <ncbphi001@gmail.com>
2023-02-07 11:00:17 +0200

    blog

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,33 +1,74 @@
 import React from 'react'
 import { getArticle } from '../../helpers/getArticles'
+import ReactMarkdown from "react-markdown";
+import Image from 'next/image';
 
 type ComponentProps = {
-  article: Article;
+  blog: Article;
 };
 
-const BlogArticle = ({article}:ComponentProps) => {
+const BlogArticle = ({blog}:ComponentProps) => {
 
-  console.log(article)
-
+console.log({blog})
   return (
-    <div>Blog Article</div>
-  )
+    <main>
+      <div className="max-w-7xl mx-auto px-4 lg:px-0 text-slate-700 mb-6">
+        <div className="flex gap-4 mb-4">
+          <Image
+            src={blog.attributes.main_image.data.attributes.formats.large.url}
+            width={
+              blog.attributes.main_image.data.attributes.formats.large.width
+            }
+            height={
+              blog.attributes.main_image.data.attributes.formats.large.height
+            }
+            alt={blog.attributes.main_image.data.attributes.name}
+            className="w-full sm:w-2/3 md:w-1/2 aspect-video"
+          />{" "}
+          <h1 className=" text-xl md:text-3xl font-bold">
+            {blog.attributes.title}
+          </h1>
+        </div>
+
+        <ReactMarkdown children={blog.attributes.body} />
+      </div>
+    </main>
+  );
 }
 
 export default BlogArticle
 
 
-async function getServerSideProps({params: {id}}:{params: {id:string}}) {
+ export const getStaticPaths = async () => {
+   const res = await fetch(
+     "https://momentous-wacky-advice.strapiapp.com/api/blogs?populate=*"
+   );
+   const { data: articles } = await res.json();
 
-  const url = process.env.NEXT_PUBLIC_STRAPI_URL;
-
-   const response =  await fetch(`${url}/api/blogs/${id}`)
-
-   const article = response.json()
+   const paths = articles.map((article: Article) => {
+     return {
+       params: {
+         id: article.id.toString(),
+       },
+     };
+   });
 
    return {
-    props: {
-      article
-    }
-   }
-}
+     paths,
+     fallback: false,
+   };
+ };
+
+
+    export const getStaticProps = async ({ params : {id}}: { params: {id: string}}) => {
+
+      const res = await fetch(
+        `https://momentous-wacky-advice.strapiapp.com/api/blogs/${id}?populate=*`
+      );
+      const { data: blog } = await res.json();
+
+      return {
+        props: { blog },
+        revalidate: 1,
+      };
+    };
