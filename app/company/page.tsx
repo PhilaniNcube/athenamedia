c504295ff3d1b53cd672e080fff9bc675d06b1ac
Philani Ncube <ncbphi001@gmail.com>
2023-03-12 22:52:32 +0200

    about us metadata

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -3,6 +3,12 @@ import CompanySectionOne from "../../components/Company/CompanySectionOne";
 import CompanySectionTwo from "../../components/Company/CompanySectionTwo";
 import GetInTouch from "../../components/GetInTouch";
 
+export const metadata = {
+  title: "Contact Us",
+  description:
+    "Discover Our Expertise in Crafting Custom Websites | Learn About Our Passion for Design, Development, and Innovation | Trust Our Experienced Team to Bring Your Vision to Life",
+};
+
 const page = () => {
   return (
     <main>
